7601b77138e4980032c9574c6cde485a651ba846
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-09 11:53:38 +0600

    FeedBack Section Added

---
+++ src/Components/FeedBack.js
@@ -0,0 +1,28 @@
+import { Button } from "@material-tailwind/react";
+import React from "react";
+import { FaQuoteRight } from "react-icons/fa";
+
+const FeedBack = ({ img, name, work, feedback }) => {
+    return (
+        <div className="bg-black/5 rounded-lg py-8 xs:py-10 md:py-12 px-6 sm:px-8">
+            <FaQuoteRight className="text-5xl mx-auto mb-5 text-black/50" />
+            <blockquote>
+                <p>{feedback}</p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                <img className="w-6 h-6 rounded-full" src={img} alt="" />
+                <div className="flex flex-col md:flex-row items-center md:divide-x-2 divide-base-content/50">
+                    <div className="pr-3 font-medium">{name}</div>
+                    <div className="pl-3 text-sm font-light text-base-content/50">{work}</div>
+                </div>
+            </figcaption>
+            <div className="flex justify-center mt-4">
+                <Button variant="gradient" size="sm" color="gray">
+                    Edit
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default FeedBack;
--- src/Pages/DynamicService.js
+++ src/Pages/DynamicService.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import SetTitle from "../Components/SetTitle";
+import FeedBack from "../Components/FeedBack";
 
 const DynamicService = () => {
     const [category, setCategory] = useState({});
@@ -28,12 +29,12 @@ const DynamicService = () => {
                 </Link>
                 <Link>{name}</Link>
             </Header>
-            <section className="my-container section-gap grid grid-cols-2 items-center gap-12">
-                <div>
-                    <img src={img} alt="" />
+            <section className="my-container section-gap grid grid-cols-1 lg:grid-cols-2 items-center gap-6 xs:gap-10 xl:gap-12">
+                <div className="w-full sm:max-w-md mx-auto lg:max-w-none">
+                    <img className="rounded-lg bg-black/5" src={img} alt="" />
                 </div>
-                <div className="flex flex-col gap-2">
-                    <h3>{name}</h3>
+                <div className="flex flex-col gap-2 bg-black/5 p-6 xl:px-8 xl:py-10 rounded-lg">
+                    <h3 className="p-4 shadow rounded-md mb-2">{name}</h3>
                     <h4>Price: {price}$</h4>
                     <h4>Rating: {ratings}</h4>
                     <p>
@@ -44,6 +45,35 @@ const DynamicService = () => {
                         Book This Services
                     </Button>
                 </div>
+            </section>
+            <section className="my-container mb-12 sm:mb-14 lg:mb-16">
+                <h1 className="text-center mb-10">Customer Feedback</h1>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 xs:gap-10 xl:gap-12">
+                    <FeedBack
+                        name={"MD Rofiq"}
+                        work={"Web DeveLoper At Facebook"}
+                        feedback={
+                            "My experience at Madroos is great and memorable. Huge respect, love and devotion for entire faculty members and department. It's their efforts that make me to count myself into better professionals."
+                        }
+                    />
+                    <FeedBack
+                        name={"MD Rofiq"}
+                        work={"Web DeveLoper At Facebook"}
+                        feedback={
+                            "My experience at Madroos is great and memorable. Huge respect, love and devotion for entire faculty members and department. It's their efforts that make me to count myself into better professionals."
+                        }
+                    />
+                    <FeedBack
+                        name={"MD Rofiq"}
+                        work={"Web DeveLoper At Facebook"}
+                        feedback={
+                            "My experience at Madroos is great and memorable. Huge respect, love and devotion for entire faculty members and department. It's their efforts that make me to count myself into better professionals."
+                        }
+                    />
+                </div>
+                <Link className="flex justify-center mt-10">
+                    <Button variant="gradient">Add Your FeedBack</Button>
+                </Link>
             </section>
         </>
     );
